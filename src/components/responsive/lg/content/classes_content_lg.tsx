b171e84b6f05cf0a6fa1f4c82f0a8bf3a12fb3f7
bostondebateacademy <113135246+bostondebateacademy@users.noreply.github.com>
2022-09-12 23:41:36 -0400

    Update classes_content_lg.tsx

--- src/components/responsive/lg/content/classes_content_lg.tsx
+++ src/components/responsive/lg/content/classes_content_lg.tsx
@@ -42,7 +42,7 @@ export default function ClassesContentLG() {
                         Classes start October 10th.
                     </p>
                     <p style={{color: "white", textAlign: "center", marginTop: "-20px"}}>
-                        (want a try-out first? Click "join us" and let us know!)
+                        (All classes are online for our Fall 2022 semester. Want a try-out first? Click "join us" and let us know!)
                     </p>
                 </Col>
             </Row>
